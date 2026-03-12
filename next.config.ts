fcd891d6b1c195ef290a0746a4a8c0d79be0a385
LongStrideTheGreat <hermanfranken2@hotmail.com>
2026-03-12 20:18:43 +0200

    Fix firebase-admin runtime on Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-
-  
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = [...(config.externals || []), 'firebase-admin'];
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
-
